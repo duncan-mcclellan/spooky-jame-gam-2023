46f74ec7660df567a42e38dab02a24d9b09a25af
duncan-mcclellan <82678263+duncan-mcclellan@users.noreply.github.com>
2023-10-28 09:53:12 -0500

    Fixing build

--- Source/SpookyJameGam2023/Private/Character/AI/EnemyController.cpp
+++ Source/SpookyJameGam2023/Private/Character/AI/EnemyController.cpp
@@ -1,73 +1,73 @@
-// Include necessary headers
-#include "Character/AI/EnemyController.h"
-#include "Perception/AIPerceptionComponent.h"
-#include "Perception/AISenseConfig_Sight.h"
-#include "NavigationSystem/Public/NavigationSystem.h"
-#include "Character/SJGCharacterBase.h"
-#include <Kismet/KismetMathLibrary.h>
-#include "NavigationSystem.h"
+// // Include necessary headers
+// #include "Character/AI/EnemyController.h"
+// #include "Perception/AIPerceptionComponent.h"
+// #include "Perception/AISenseConfig_Sight.h"
+// #include "NavigationSystem/Public/NavigationSystem.h"
+// #include "Character/SJGCharacterBase.h"
+// #include <Kismet/KismetMathLibrary.h>
+// #include "NavigationSystem.h"
 
 
 
-AEnemyController::AEnemyController()
-{
-    PrimaryActorTick.bCanEverTick = true;
+// AEnemyController::AEnemyController()
+// {
+//     PrimaryActorTick.bCanEverTick = true;
 
 
-}
+// }
 
-void AEnemyController::BeginPlay()
-{
-	Super::BeginPlay();
-
-
-
-}
+// void AEnemyController::BeginPlay()
+// {
+// 	Super::BeginPlay();
 
 
 
-void AEnemyController::Tick(float DeltaSeconds)
-{
-	Super::Tick(DeltaSeconds);
+// }
 
 
-}
+
+// void AEnemyController::Tick(float DeltaSeconds)
+// {
+// 	Super::Tick(DeltaSeconds);
 
 
-void AEnemyController::MoveToRandomLocationAndLookAround()
-{
-    const FVector Origin = GetPawn()->GetActorLocation();
-    const float Radius = 1000.0f;
-    const FVector RandomLocation = UKismetMathLibrary::RandomPointInBoundingBox(Origin, FVector(Radius, Radius, 10.0f));
+// }
 
-    // Use AI's built-in pathfinding to move the character to the random location.
-    UNavigationSystemV1* NavSys = FNavigationSystem::GetCurrent<UNavigationSystemV1>(GetWorld());
-    if (NavSys)
-    {
-        FNavLocation OutLocation;
-        if (NavSys->GetRandomPointInNavigableRadius(RandomLocation, Radius, OutLocation))
-        {
-            MoveToLocation(OutLocation.Location);
-        }
-    }
 
-    // Pause for 3 seconds using a timer.
-    GetWorldTimerManager().SetTimer(TimerHandle_LookAround, this, &AEnemyController::OnLookAroundComplete, 3.0f, false);
-}
+// void AEnemyController::MoveToRandomLocationAndLookAround()
+// {
+//     const FVector Origin = GetPawn()->GetActorLocation();
+//     const float Radius = 1000.0f;
+//     const FVector RandomLocation = UKismetMathLibrary::RandomPointInBoundingBox(Origin, FVector(Radius, Radius, 10.0f));
 
-void AEnemyController::OnLookAroundComplete()
-{
+//     // Use AI's built-in pathfinding to move the character to the random location.
+//     UNavigationSystemV1* NavSys = FNavigationSystem::GetCurrent<UNavigationSystemV1>(GetWorld());
+//     if (NavSys)
+//     {
+//         FNavLocation OutLocation;
+//         //if (NavSys->GetRandomPointInNavigableRadius(RandomLocation, Radius, OutLocation))
+//         //{
+//             //MoveToLocation(OutLocation.Location);
+//         //}
+//     }
 
-    MoveToRandomLocationAndLookAround();
-}
+//     // Pause for 3 seconds using a timer.
+//     GetWorldTimerManager().SetTimer(TimerHandle_LookAround, this, &AEnemyController::OnLookAroundComplete, 3.0f, false);
+// }
 
-void AEnemyController::EnteringScaryMode()
-{
+// void AEnemyController::OnLookAroundComplete()
+// {
+
+//     MoveToRandomLocationAndLookAround();
+// }
+
+// void AEnemyController::EnteringScaryMode()
+// {
    
-}
+// }
 
-void AEnemyController::MoveToASafeLocation()
-{
-}
+// void AEnemyController::MoveToASafeLocation()
+// {
+// }
 
 
--- Source/SpookyJameGam2023/Public/Character/AI/EnemyController.h
+++ Source/SpookyJameGam2023/Public/Character/AI/EnemyController.h
@@ -1,36 +1,36 @@
-#pragma once
+// #pragma once
 
-#include "CoreMinimal.h"
-#include "AIController.h"
-#include "NPCS.h"
-#include "Perception/AIPerceptionTypes.h"
-#include "Perception/AISenseConfig_Sight.h"
-#include "EnemyController.generated.h"
+// #include "CoreMinimal.h"
+// #include "AIController.h"
+// #include "NPCS.h"
+// #include "Perception/AIPerceptionTypes.h"
+// #include "Perception/AISenseConfig_Sight.h"
+// #include "EnemyController.generated.h"
 
-UCLASS()
-class SPOOKYJAMEGAM2023_API AEnemyController : public AAIController
-{
-    GENERATED_BODY()
+// UCLASS()
+// class SPOOKYJAMEGAM2023_API AEnemyController : public AAIController
+// {
+//     GENERATED_BODY()
 
-protected:
-    virtual void BeginPlay() override;
-    virtual void Tick(float DeltaSeconds) override;
+// protected:
+//     virtual void BeginPlay() override;
+//     virtual void Tick(float DeltaSeconds) override;
 
-public:
-    AEnemyController();
+// public:
+//     AEnemyController();
 
-public:
-    TObjectPtr<ANPCS>Npc;
+// public:
+//     TObjectPtr<ANPCS>Npc;
 
-    FTimerHandle TimerHandle_LookAround;
+//     FTimerHandle TimerHandle_LookAround;
 
 
-    UFUNCTION()
-	void MoveToRandomLocationAndLookAround();
+//     UFUNCTION()
+// 	void MoveToRandomLocationAndLookAround();
 
-    void OnLookAroundComplete();
+//     void OnLookAroundComplete();
 
-    void EnteringScaryMode();
+//     void EnteringScaryMode();
 
-    void MoveToASafeLocation();
-};
+//     void MoveToASafeLocation();
+// };
